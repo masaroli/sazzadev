b6e0e6517c7e1773f4c6ca5907706e28ff61a874
Martin Sazzaroli <martinsazzaroli@gmail.com>
2024-02-26 19:56:27 -0300

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [icon()],
   devToolbar: { enabled: false },
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
