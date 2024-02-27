aae7f585ccb1832c326dcdc98fcc461f0c098912
Martin Sazzaroli <martinsazzaroli@gmail.com>
2024-02-26 22:04:11 -0300

    Fix build

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_nGqGOj2_.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Y-m8QnIK.mjs').then(n => n.a);
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [icon()],
   devToolbar: { enabled: false },
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
